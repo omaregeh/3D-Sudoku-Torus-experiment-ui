8fbdc245abda1b1df35626d64a6894203ad242d4
omaregeh <omarhajiegeh@gmail.com>
2024-10-27 02:52:40 -0400

    Changed lighting intensity

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Lighting
     const ambientLight = new THREE.AmbientLight(0xffffff, 1);
     scene.add(ambientLight);
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.1);
     directionalLight.position.set(15, 25, 15);  // Moved light further back
     scene.add(directionalLight);
 
